4ce91aa92012051dee45e20f720419abfbefad98
danembb <dbrophy720@gmail.com>
2021-06-03 10:18:49 -0600

    Create add_ship method for flotilla class

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -2,10 +2,14 @@ class Flotilla
   attr_reader :name,
               :personnel,
               :ships
-              
+
   def initialize(flotilla_info)
     @name = flotilla_info[:designation]
     @personnel = []
     @ships = []
+  end
+
+  def add_ship(ship)
+    @ships.push(ship)
   end
 end
--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -12,11 +12,22 @@ RSpec.describe Flotilla do
     end
 
     it 'has attributes' do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: '400'})
       seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
 
       expect(seventh_flotilla.name).to eq("Seventh Flotilla")
       expect(seventh_flotilla.personnel).to eq([])
       expect(seventh_flotilla.ships).to eq([])
+    end
+  end
+
+  describe 'methods' do
+    it '#add_ship can add a ship to ships' do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: '400'})
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+      seventh_flotilla.add_ship(daedalus)
+
+      expect(seventh_flotilla.ships).to eq([daedalus])
     end
   end
 end
