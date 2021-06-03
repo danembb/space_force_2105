f217b73290216b588f4c4502de4c8808eff74c1d
danembb <dbrophy720@gmail.com>
2021-06-03 09:19:01 -0600

    Test attributes for spacecraft class

--- lib/spacecraft.rb
+++ lib/spacecraft.rb
@@ -1,5 +1,9 @@
 class Spacecraft
+  attr_reader :name,
+              :fuel
 
   def initialize(spacecraft_info)
+    @name = spacecraft_info[:name]
+    @fuel = spacecraft_info[:fuel].to_i
   end
 end
--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe Spacecraft do
 
       expect(daedalus).to be_a(Spacecraft)
     end
+
+    it 'has attributes' do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: '400'})
+
+      expect(daedalus.name).to eq('Daedalus')
+      expect(daedalus.fuel).to eq(400)
+    end
   end
 end
