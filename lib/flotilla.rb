c5e33d1eda26b0dcb049c19dae537dd5007a44f0
danembb <dbrophy720@gmail.com>
2021-06-03 10:57:49 -0600

    In progress recommend_personnel method for flotilla class

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -16,4 +16,18 @@ class Flotilla
   def add_personnel(personnel)
     @personnel.push(personnel)
   end
+
+#This needs to look into personnel array, which needs to
+#look into the specialties array and compare specialties
+#against the spacecraft's requirements array.
+  def recommend_personnel(ship)
+    recommended_people = []
+    @personnel.each do |personnel|
+    personnel.specialties.each do |specialty|
+      ship.requirements.include?(specialty)
+        recommended_people.push(personnel)
+      end
+    end
+    return recommended_people.uniq
+  end
 end
