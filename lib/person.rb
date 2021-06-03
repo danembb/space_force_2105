324f8bf4c3e707174aecf9a39fb53cffcc44e8ae
danembb <dbrophy720@gmail.com>
2021-06-03 09:38:58 -0600

    Test attributes for person class

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,11 @@
 class Person
+  attr_reader :name,
+              :experience,
+              :specialties
 
   def initialize(name, experience)
+    @name = name
+    @experience = experience
+    @specialties = []
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe Person do
 
       expect(kathy).to be_a(Person)
     end
+
+    it 'has attributes' do
+      kathy = Person.new('Kathy Chan', 10)
+
+      expect(kathy.name).to eq('Kathy Chan')
+      expect(kathy.experience).to eq(10)
+      expect(kathy.specialties).to eq([])
+    end
   end
 end
