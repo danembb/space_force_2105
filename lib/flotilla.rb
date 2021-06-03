2a85576693bf1c2614e423b489dc5a8a4eca7ac2
danembb <dbrophy720@gmail.com>
2021-06-03 10:13:55 -0600

    Create flotilla class

---
+++ lib/flotilla.rb
@@ -0,0 +1,5 @@
+class Flotilla
+
+  def initialize(flotilla_info)
+  end
+end
--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -1,9 +1,14 @@
-require './lib/floatilla'
+require './lib/flotilla'
+require './lib/spacecraft'
+require './lib/person'
 
-RSpec.describe Floatilla do
+RSpec.describe Flotilla do
   describe 'instantiation' do
     it '::new' do
       daedalus = Spacecraft.new({name: 'Daedalus', fuel: '400'})
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+
+      expect(seventh_flotilla).to be_a(Flotilla)
     end
   end
 end
