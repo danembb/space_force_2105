d94cd698188a3527c173b15a49c88b7602608ffd
danembb <dbrophy720@gmail.com>
2021-06-03 10:16:42 -0600

    Test attributes for flotilla class

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -1,5 +1,11 @@
 class Flotilla
-
+  attr_reader :name,
+              :personnel,
+              :ships
+              
   def initialize(flotilla_info)
+    @name = flotilla_info[:designation]
+    @personnel = []
+    @ships = []
   end
 end
--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe Flotilla do
 
       expect(seventh_flotilla).to be_a(Flotilla)
     end
+
+    it 'has attributes' do
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+
+      expect(seventh_flotilla.name).to eq("Seventh Flotilla")
+      expect(seventh_flotilla.personnel).to eq([])
+      expect(seventh_flotilla.ships).to eq([])
+    end
   end
 end
